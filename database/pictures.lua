88507cbed611de8d7e3a12ca94e5521c7b2d8f55
Mist <114344332+MIST145@users.noreply.github.com>
2026-04-13 04:40:21 +0100

    Update pictures.lua - Added 6 new entries

---
+++ database/pictures.lua
@@ -0,0 +1,27 @@
+-- FiveManage Images
+return {
+    ["aleutian"] = {
+        label = "aleutian",
+        image = "https://raw.githubusercontent.com/MIST145/img_rup_2026/main/origen/aleutian.png"
+    },
+    ["asea"] = {
+        label = "asea",
+        image = "https://raw.githubusercontent.com/MIST145/img_rup_2026/main/origen/asea.png"
+    },
+    ["asea2"] = {
+        label = "asea2",
+        image = "https://raw.githubusercontent.com/MIST145/img_rup_2026/main/origen/asea2.png"
+    },
+    ["asterope"] = {
+        label = "asterope",
+        image = "https://raw.githubusercontent.com/MIST145/img_rup_2026/main/origen/asterope.png"
+    },
+    ["asterope2"] = {
+        label = "asterope2",
+        image = "https://raw.githubusercontent.com/MIST145/img_rup_2026/main/origen/asterope2.png"
+    },
+    ["astron"] = {
+        label = "astron",
+        image = "https://raw.githubusercontent.com/MIST145/img_rup_2026/main/origen/astron.png"
+    }
+}
