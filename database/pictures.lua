36e9ca4723cf882e71f25e21c92653981b2595aa
Mist <114344332+MIST145@users.noreply.github.com>
2026-04-23 01:42:12 +0100

    Update pictures.lua - Added 69 new entries

--- database/pictures.lua
+++ database/pictures.lua
@@ -55,5 +55,277 @@ return {
     ["m3cst"] = {
         label = "m3cst",
         image = "https://raw.githubusercontent.com/MIST145/img_rup_2026/main/origen/m3cst.png"
+    },
+    ["gstsulrs1"] = {
+        label = "gstsulrs1",
+        image = "https://raw.githubusercontent.com/MIST145/img_rup_2026/main/2026/april/gstsulrs1.png"
+    },
+    ["gstzen1"] = {
+        label = "gstzen1",
+        image = "https://raw.githubusercontent.com/MIST145/img_rup_2026/main/2026/april/gstzen1.png"
+    },
+    ["gxa45"] = {
+        label = "gxa45",
+        image = "https://raw.githubusercontent.com/MIST145/img_rup_2026/main/2026/april/gxa45.png"
+    },
+    ["hachurac"] = {
+        label = "hachurac",
+        image = "https://raw.githubusercontent.com/MIST145/img_rup_2026/main/2026/april/hachurac.png"
+    },
+    ["hycsun"] = {
+        label = "hycsun",
+        image = "https://raw.githubusercontent.com/MIST145/img_rup_2026/main/2026/april/hycsun.png"
+    },
+    ["infernuswb"] = {
+        label = "infernuswb",
+        image = "https://raw.githubusercontent.com/MIST145/img_rup_2026/main/2026/april/infernuswb.png"
+    },
+    ["jestgpr"] = {
+        label = "jestgpr",
+        image = "https://raw.githubusercontent.com/MIST145/img_rup_2026/main/2026/april/jestgpr.png"
+    },
+    ["lynxgpr"] = {
+        label = "lynxgpr",
+        image = "https://raw.githubusercontent.com/MIST145/img_rup_2026/main/2026/april/lynxgpr.png"
+    },
+    ["mgt"] = {
+        label = "mgt",
+        image = "https://raw.githubusercontent.com/MIST145/img_rup_2026/main/2026/april/mgt.png"
+    },
+    ["nriata"] = {
+        label = "nriata",
+        image = "https://raw.githubusercontent.com/MIST145/img_rup_2026/main/2026/april/nriata.png"
+    },
+    ["nriata2"] = {
+        label = "nriata2",
+        image = "https://raw.githubusercontent.com/MIST145/img_rup_2026/main/2026/april/nriata2.png"
+    },
+    ["oracxsle"] = {
+        label = "oracxsle",
+        image = "https://raw.githubusercontent.com/MIST145/img_rup_2026/main/2026/april/oracxsle.png"
+    },
+    ["osirisfr"] = {
+        label = "osirisfr",
+        image = "https://raw.githubusercontent.com/MIST145/img_rup_2026/main/2026/april/osirisfr.png"
+    },
+    ["paragonxr"] = {
+        label = "paragonxr",
+        image = "https://raw.githubusercontent.com/MIST145/img_rup_2026/main/2026/april/paragonxr.png"
+    },
+    ["pentro"] = {
+        label = "pentro",
+        image = "https://raw.githubusercontent.com/MIST145/img_rup_2026/main/2026/april/pentro.png"
+    },
+    ["pentro2"] = {
+        label = "pentro2",
+        image = "https://raw.githubusercontent.com/MIST145/img_rup_2026/main/2026/april/pentro2.png"
+    },
+    ["pentro3"] = {
+        label = "pentro3",
+        image = "https://raw.githubusercontent.com/MIST145/img_rup_2026/main/2026/april/pentro3.png"
+    },
+    ["pentrogpr"] = {
+        label = "pentrogpr",
+        image = "https://raw.githubusercontent.com/MIST145/img_rup_2026/main/2026/april/pentrogpr.png"
+    },
+    ["pentrogpr2"] = {
+        label = "pentrogpr2",
+        image = "https://raw.githubusercontent.com/MIST145/img_rup_2026/main/2026/april/pentrogpr2.png"
+    },
+    ["radiantvff"] = {
+        label = "radiantvff",
+        image = "https://raw.githubusercontent.com/MIST145/img_rup_2026/main/2026/april/radiantvff.png"
+    },
+    ["reagpr"] = {
+        label = "reagpr",
+        image = "https://raw.githubusercontent.com/MIST145/img_rup_2026/main/2026/april/reagpr.png"
+    },
+    ["remustwo"] = {
+        label = "remustwo",
+        image = "https://raw.githubusercontent.com/MIST145/img_rup_2026/main/2026/april/remustwo.png"
+    },
+    ["remuswb"] = {
+        label = "remuswb",
+        image = "https://raw.githubusercontent.com/MIST145/img_rup_2026/main/2026/april/remuswb.png"
+    },
+    ["rh4"] = {
+        label = "rh4",
+        image = "https://raw.githubusercontent.com/MIST145/img_rup_2026/main/2026/april/rh4.png"
+    },
+    ["roxanne"] = {
+        label = "roxanne",
+        image = "https://raw.githubusercontent.com/MIST145/img_rup_2026/main/2026/april/roxanne.png"
+    },
+    ["schlagenstr"] = {
+        label = "schlagenstr",
+        image = "https://raw.githubusercontent.com/MIST145/img_rup_2026/main/2026/april/schlagenstr.png"
+    },
+    ["sheavas"] = {
+        label = "sheavas",
+        image = "https://raw.githubusercontent.com/MIST145/img_rup_2026/main/2026/april/sheavas.png"
+    },
+    ["skyline"] = {
+        label = "skyline",
+        image = "https://raw.githubusercontent.com/MIST145/img_rup_2026/main/2026/april/skyline.png"
+    },
+    ["slsamg"] = {
+        label = "slsamg",
+        image = "https://raw.githubusercontent.com/MIST145/img_rup_2026/main/2026/april/slsamg.png"
+    },
+    ["stockade4"] = {
+        label = "stockade4",
+        image = "https://raw.githubusercontent.com/MIST145/img_rup_2026/main/2026/april/stockade4.png"
+    },
+    ["sultanrsv8"] = {
+        label = "sultanrsv8",
+        image = "https://raw.githubusercontent.com/MIST145/img_rup_2026/main/2026/april/sultanrsv8.png"
+    },
+    ["sunrise1"] = {
+        label = "sunrise1",
+        image = "https://raw.githubusercontent.com/MIST145/img_rup_2026/main/2026/april/sunrise1.png"
+    },
+    ["tempesta2"] = {
+        label = "tempesta2",
+        image = "https://raw.githubusercontent.com/MIST145/img_rup_2026/main/2026/april/tempesta2.png"
+    },
+    ["tempestaes"] = {
+        label = "tempestaes",
+        image = "https://raw.githubusercontent.com/MIST145/img_rup_2026/main/2026/april/tempestaes.png"
+    },
+    ["tmax"] = {
+        label = "tmax",
+        image = "https://raw.githubusercontent.com/MIST145/img_rup_2026/main/2026/april/tmax.png"
+    },
+    ["torrence"] = {
+        label = "torrence",
+        image = "https://raw.githubusercontent.com/MIST145/img_rup_2026/main/2026/april/torrence.png"
+    },
+    ["vigerozxwb"] = {
+        label = "vigerozxwb",
+        image = "https://raw.githubusercontent.com/MIST145/img_rup_2026/main/2026/april/vigerozxwb.png"
+    },
+    ["wide296"] = {
+        label = "wide296",
+        image = "https://raw.githubusercontent.com/MIST145/img_rup_2026/main/2026/april/wide296.png"
+    },
+    ["zr390"] = {
+        label = "zr390",
+        image = "https://raw.githubusercontent.com/MIST145/img_rup_2026/main/2026/april/zr390.png"
+    },
+    ["zrgpr"] = {
+        label = "zrgpr",
+        image = "https://raw.githubusercontent.com/MIST145/img_rup_2026/main/2026/april/zrgpr.png"
+    },
+    ["25pininfarina"] = {
+        label = "25pininfarina",
+        image = "https://raw.githubusercontent.com/MIST145/img_rup_2026/main/2026/april/25pininfarina.png"
+    },
+    ["69charger"] = {
+        label = "69charger",
+        image = "https://raw.githubusercontent.com/MIST145/img_rup_2026/main/2026/april/69charger.png"
+    },
+    ["440i"] = {
+        label = "440i",
+        image = "https://raw.githubusercontent.com/MIST145/img_rup_2026/main/2026/april/440i.png"
+    },
+    ["aa_pr911_992_21"] = {
+        label = "aa_pr911_992_21",
+        image = "https://raw.githubusercontent.com/MIST145/img_rup_2026/main/2026/april/aa_pr911_992_21.png"
+    },
+    ["apollo"] = {
+        label = "apollo",
+        image = "https://raw.githubusercontent.com/MIST145/img_rup_2026/main/2026/april/apollo.png"
+    },
+    ["audace"] = {
+        label = "audace",
+        image = "https://raw.githubusercontent.com/MIST145/img_rup_2026/main/2026/april/audace.png"
+    },
+    ["auroras"] = {
+        label = "auroras",
+        image = "https://raw.githubusercontent.com/MIST145/img_rup_2026/main/2026/april/auroras.png"
+    },
+    ["auroras2"] = {
+        label = "auroras2",
+        image = "https://raw.githubusercontent.com/MIST145/img_rup_2026/main/2026/april/auroras2.png"
+    },
+    ["bansheeas"] = {
+        label = "bansheeas",
+        image = "https://raw.githubusercontent.com/MIST145/img_rup_2026/main/2026/april/bansheeas.png"
+    },
+    ["beretta"] = {
+        label = "beretta",
+        image = "https://raw.githubusercontent.com/MIST145/img_rup_2026/main/2026/april/beretta.png"
+    },
+    ["blista2"] = {
+        label = "blista2",
+        image = "https://raw.githubusercontent.com/MIST145/img_rup_2026/main/2026/april/blista2.png"
+    },
+    ["bmwm3e92"] = {
+        label = "bmwm3e92",
+        image = "https://raw.githubusercontent.com/MIST145/img_rup_2026/main/2026/april/bmwm3e92.png"
+    },
+    ["comet6scw"] = {
+        label = "comet6scw",
+        image = "https://raw.githubusercontent.com/MIST145/img_rup_2026/main/2026/april/comet6scw.png"
+    },
+    ["coquette4c"] = {
+        label = "coquette4c",
+        image = "https://raw.githubusercontent.com/MIST145/img_rup_2026/main/2026/april/coquette4c.png"
+    },
+    ["coquettewb"] = {
+        label = "coquettewb",
+        image = "https://raw.githubusercontent.com/MIST145/img_rup_2026/main/2026/april/coquettewb.png"
+    },
+    ["cypherwb"] = {
+        label = "cypherwb",
+        image = "https://raw.githubusercontent.com/MIST145/img_rup_2026/main/2026/april/cypherwb.png"
+    },
+    ["deluxowb"] = {
+        label = "deluxowb",
+        image = "https://raw.githubusercontent.com/MIST145/img_rup_2026/main/2026/april/deluxowb.png"
+    },
+    ["draftgpr"] = {
+        label = "draftgpr",
+        image = "https://raw.githubusercontent.com/MIST145/img_rup_2026/main/2026/april/draftgpr.png"
+    },
+    ["dubstab"] = {
+        label = "dubstab",
+        image = "https://raw.githubusercontent.com/MIST145/img_rup_2026/main/2026/april/dubstab.png"
+    },
+    ["elegyrh6"] = {
+        label = "elegyrh6",
+        image = "https://raw.githubusercontent.com/MIST145/img_rup_2026/main/2026/april/elegyrh6.png"
+    },
+    ["elegyrh7"] = {
+        label = "elegyrh7",
+        image = "https://raw.githubusercontent.com/MIST145/img_rup_2026/main/2026/april/elegyrh7.png"
+    },
+    ["elegyrh72"] = {
+        label = "elegyrh72",
+        image = "https://raw.githubusercontent.com/MIST145/img_rup_2026/main/2026/april/elegyrh72.png"
+    },
+    ["flatbed3"] = {
+        label = "flatbed3",
+        image = "https://raw.githubusercontent.com/MIST145/img_rup_2026/main/2026/april/flatbed3.png"
+    },
+    ["furiawb"] = {
+        label = "furiawb",
+        image = "https://raw.githubusercontent.com/MIST145/img_rup_2026/main/2026/april/furiawb.png"
+    },
+    ["gauntletstx"] = {
+        label = "gauntletstx",
+        image = "https://raw.githubusercontent.com/MIST145/img_rup_2026/main/2026/april/gauntletstx.png"
+    },
+    ["gp1wb"] = {
+        label = "gp1wb",
+        image = "https://raw.githubusercontent.com/MIST145/img_rup_2026/main/2026/april/gp1wb.png"
+    },
+    ["gstsul1"] = {
+        label = "gstsul1",
+        image = "https://raw.githubusercontent.com/MIST145/img_rup_2026/main/2026/april/gstsul1.png"
+    },
+    ["ST3"] = {
+        label = "ST3",
+        image = "https://raw.githubusercontent.com/MIST145/img_rup_2026/main/2026/april/ST3.png"
     }
 }
